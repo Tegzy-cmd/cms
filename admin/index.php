a92c5d98959fa7ef6ce5ad187c7cac0e2b0323d2
Tegzy-cmd <77752563+Tegzy-cmd@users.noreply.github.com>
2022-10-25 00:27:22 +0100

    CMS project... currently working on the admin

---
+++ admin/index.php
@@ -0,0 +1,38 @@
+
+<!-- Header -->
+
+    <?php include("includes/header.php") ?>
+
+<!-- / Header -->
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include("includes/nav.php") ?>
+        <!-- /Navigation -->
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to admin panel
+                            <small>Author</small>
+                        </h1>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+        <!-- footer -->
+        <?php include("includes/footer.php") ?>
+        <!-- / footer -->
